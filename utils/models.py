3a5316b07cc321865673c5fb7232a43f30b39acb
hidyg <gabor.hidy@gmail.com>
2024-09-06 12:57:30 +0200

    Added transformer encoder

--- utils/models.py
+++ utils/models.py
@@ -1,7 +1,8 @@
+import numpy as np
 import torch.nn as nn  # construct NN
 import torch.nn.functional as F
 import torch
-from typing import Dict,  Tuple, List, Union
+from typing import Dict, Literal,  Tuple, List, Union
 
 
 
@@ -183,8 +184,76 @@ class RNNEncoder(nn.Module):
         y_hat = self.activ_loss(self.cl1(H))  # activ_loss is activation directly before feeding to loss
         return y_hat, H, [torch.zeros(2,3)] # no activations returned
 
+class TransformerEncoder(nn.Module):
 
+    def __init__(self,
+                 input_size : int,
+                 hidden_size : int,
+                 dim_model : int = 64,
+                 dim_ff : int = 128,
+                 n_heads : int = 1,
+                 num_layers : int = 8,
+                 dropout : float = 0.1,
+                 activations : Dict[str, nn.Module] = dict(H = nn.LeakyReLU(0.2), loss = nn.Sigmoid()),
+                 positional_encoding : Literal['none', 'sinusoidal', 'trainable'] = 'sinusoidal',
+                 max_input_len : int = 1024
+                 ):
+        
+        super().__init__()
 
+        self.embed = nn.Linear(input_size, dim_model)
+        self.transformer = nn.TransformerEncoder(nn.TransformerEncoderLayer(d_model = dim_model,
+                                                                            dim_feedforward = dim_ff,
+                                                                            nhead = n_heads,
+                                                                            dropout = dropout,
+                                                                            batch_first = True,
+                                                                            activation = 'gelu'),
+                                                 num_layers = num_layers)
+        self.unembed = nn.Linear(dim_model, hidden_size)
+        self.activation = activations['H']
+        self.head = HiddenElementsFinalLayer(hidden_size, map_element_size = 1)
+        self.final_act = activations['loss']
+
+        if positional_encoding == 'none':
+            self.pos_enc = nn.Parameter(torch.zeros(max_input_len, dim_model), requires_grad = False)
+        elif positional_encoding == 'sinusoidal':
+            def calc_pos_enc(i, j, N = 10000):
+                n = N ** (1 / dim_model)
+                if j % 2 == 0:
+                    return np.sin(i / (n**j))
+                else:
+                    return np.cos(i / (n**(j-1)))
+            pos_enc = torch.empty(max_input_len, dim_model)
+            for i in range(max_input_len):
+                for j in range(dim_model):
+                    pos_enc[i, j] = calc_pos_enc(i, j)
+            self.pos_enc = nn.Parameter(pos_enc, requires_grad = False)
+        elif positional_encoding == 'trainable':
+            self.pos_enc = nn.Parameter(torch.empty(max_input_len, dim_model), requires_grad = True)
+            self.init_weights(self.pos_enc)
+        else:
+            raise ValueError(f'Positional encoding keyword \'{positional_encoding}\' unrecognised.')
+
+        self.apply(self.init_weights)
+    
+    def forward(self, x : torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor, List[torch.Tensor]]:
+        batch_size, seq_len, _ = x.shape
+        pos_enc = torch.tile(self.pos_enc[:seq_len], (batch_size, 1, 1))
+
+        H = self.transformer(self.embed(x) + pos_enc)
+        H = self.activation(self.unembed(H))
+
+        y_hat = self.final_act(self.head(H))
+        return y_hat, H, [torch.zeros(2, 3)]
+
+    @staticmethod
+    def init_weights(m : nn.Module):
+        try:
+            nn.init.trunc_normal_(getattr(m, 'weight', m), mean = 0, std = 0.02)
+        except AttributeError:
+            if hasattr(m, 'bias'):
+                m.bias.fill_(0.)
+        
 # wrappers:
 # encoder decoder setting
 class encoder_decoder(nn.Module):
